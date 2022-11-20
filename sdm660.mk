721a0825c111c191ca751097969cd4bc2fea6ac6
sabarop <sabar.pakpahan@gmail.com>
2022-11-20 07:24:36 +0700

    sdm660-common: Enable livedisplay

--- sdm660.mk
+++ sdm660.mk
@@ -329,6 +329,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Media
 PRODUCT_PACKAGES += \
     libgui_vendor
