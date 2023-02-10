34e9f063575e5f163759ff4dfffd3d04a21b4323
Amit Pundir <amit.pundir@linaro.org>
2023-02-10 14:15:58 +0800

    platina: seccomp: Copy mediaswcodec.policy
    * W mediaswcodec: Could not read additional policy file
    '/vendor/etc/seccomp_policy/mediaswcodec.policy'
    
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- device.mk
+++ device.mk
@@ -464,7 +464,8 @@ PRODUCT_BOOT_JARS += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(DEVICE_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(DEVICE_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
+    $(DEVICE_PATH)/seccomp/mediaswcodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
